ed6b2310d398b54cd689b82a1080700544c886f6
jaydeep <jaydesh1@gmail.com>
2017-08-13 19:21:34 -0400

    added daemonization of activity scanner

--- application_base_class.py
+++ application_base_class.py
@@ -2,6 +2,8 @@ import platform
 from datetime import datetime
 from AppKit import NSWorkspace
 import time
+import idle
+import csv
 #from Quartz.CoreGraphics import *
 #import win32gui
 
@@ -20,9 +22,24 @@ class application_properties:
 
             self.timeHistory[self.instances] = (self.name, self.starttime, self.currentTimeStampOnApplication,
                                                 self.get_time_spent(self.starttime, self.currentTimeStampOnApplication))
-            print (self.timeHistory[self.instances])
-            time.sleep(2)
+            idle_on_app = idle.get_idle_duration()
+            if idle_on_app < 300:
+                time.sleep(2)
+            else:
+                while idle.get_idle_duration() > 1:
+                    time.sleep(2)
+                break
+        self.save_time_objects()
         self.instances += 1
+
+    def save_time_objects(self):
+        with open('timeHistory.csv', 'a') as csvfile:
+            timestamp = str(datetime.today().date().month) + '/' + \
+                        str(datetime.today().date().day) + '/' + str(datetime.today().date().year)
+            fieldnames = ['timestamp', 'instance' , 'Application', 'timeHistory']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'timestamp': timestamp, 'instance': self.instances, 'Application': self.name,
+                             'timeHistory':self.timeHistory[self.instances]})
 
     @staticmethod
     def get_window_title():
@@ -44,22 +61,6 @@ class application_properties:
     def get_time_spent(t1, t2):
         time_spent = t2 - t1
         return time_spent.seconds
-    @staticmethod
-    def get_idle_duration():
-        if (platform.system() == 'Darwin'):
-            NX_ALLEVENTS = int(4294967295)  # 32-bits, all on.
-            idle = CGEventSourceSecondsSinceLastEventType(1, NX_ALLEVENTS)
-            return idle / 1000.0
-
-        else:
-            from ctypes import Structure, windll, c_uint, sizeof, byref
-            class LASTINPUTINFO(Structure):
-                _fields_ = [('cbSize', c_uint),('dwTime', c_uint),]
-            lastInputInfo = LASTINPUTINFO()
-            lastInputInfo.cbSize = sizeof(lastInputInfo)
-            windll.user32.GetLastInputInfo(byref(lastInputInfo))
-            millis = windll.kernel32.GetTickCount() - lastInputInfo.dwTime
-            return millis / 1000.0
 
 if __name__ == "__main__":
     while(1):
--- scan_activities.py
+++ scan_activities.py
@@ -2,10 +2,31 @@ import platform
 from AppKit import NSWorkspace
 from application_base_class import application_properties
 import time
+import os
+from datetime import datetime
+import csv
+
+#TODO parent directory is needed when activity tracker starts working over several days
+parent_directory = os.getcwd()
 
 class activity_scanner:
     application_names = []
     application_directory = {}
+    scanner_start_time = datetime.today()
+    storage_folder = str(scanner_start_time.year) + '-' + \
+                     str(scanner_start_time.month) + '-' + str(scanner_start_time.day)
+    storage_directory = parent_directory + '/' + storage_folder
+    if (os.path.isdir(storage_directory)) == True:
+        os.chdir(storage_directory)
+        if (os.path.isfile('timeHistory.csv')==True):
+            os.remove('timeHistory.csv')
+    else:
+        os.mkdir(storage_directory)
+        os.chdir(storage_directory)
+    with open('timeHistory.csv', 'a') as csvfile:
+        fieldnames = ['timestamp', 'instance' , 'Application', 'timeHistory']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
     @staticmethod
     def get_window_title():
         if (platform.system() == 'Darwin'):
@@ -24,10 +45,12 @@ class activity_scanner:
             activity_scanner.application_names.append(current_active)
             activity_scanner.application_directory[current_active] = application_properties(current_active)
 
-if __name__ == "__main__":
+def run():
     today = activity_scanner()
     while(1):
         today.check_if_app_exists()
         time.sleep(2)
         app = today.get_window_title()
-        #print (activity_scanner.application_directory[app].timeHistory)
+
+if __name__ == "__main__":
+    run()
